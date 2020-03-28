dd570298d7a9b143d6462984735d173c64feba16
lucky7886 <43524115+lucky7886@users.noreply.github.com>
2020-03-29 01:13:18 +0530

    kasia hai bhai

---
+++ demo.js
@@ -0,0 +1,20 @@
+const array=[1,2,3,4];
+
+const filterarray= array.filter((num)=>
+{
+	return (num>3);
+}
+
+
+
+
+	)
+ console.log(filterarray);
+
+    const reduceArray=array.reduce((acc,num)=>
+    {
+        return(acc+num);
+
+    } 5
+    );
+    console.log(reduceArray);
---
+++ vv.js
@@ -0,0 +1,10 @@
+ var user={
+
+
+ name="prakhar";
+ age=23;
+  color="brown";
+  get[]={"banana", "apples", "honda"}; 
+
+
+ }
